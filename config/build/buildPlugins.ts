ed39d51b3e6ea78e5418521bf7e758451d1d017e
Paul Gabov <pgabow@gmail.com>
2023-11-17 17:03:21 +0300

    config modification

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,12 +1,11 @@
-import HTMLWebpackPlugin from 'html-webpack-plugin'
-import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
-    new HTMLWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -10,7 +10,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
   const { paths, mode, isDev } = options
 
   return {
-    mode: mode,
+    mode,
     entry: paths.entry,
     output: {
       filename: '[name].[contenthash].js',
@@ -21,7 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   }
